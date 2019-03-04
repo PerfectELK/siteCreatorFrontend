95120a61c1d8543f5948ec8e63947c514964e99d
perfect-elk <perfect-elk@perfect-elk.ru>
2019-03-04 15:00:28 +0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,87 @@
+
+var modalDelete = Vue.component('modal-body',{
+    data:function(){
+        return{
+
+        }
+    },
+    template:'#siteDeleteForm'
+});
+
+var modalChange = Vue.component('modal-body',{
+    data:function(){
+        return{
+
+        }
+    },
+    template:'#siteChangeForm',
+});
+
+
+
+var main = Vue.component('main-c',{
+    data:function () {
+        return{
+            modalClose:true,
+            currentModal:'modalDelete',
+        }
+    },
+    components:{
+        'modalDelete':modalDelete,
+        'modalChange':modalChange,
+    },
+    methods:{
+        getModal:function(){
+            this.modalClose = false;
+        },
+        changeSiteModal:function(siteName){
+            this.currentModal = 'modalChange';
+            this.getModal();
+        },
+        deleteSiteModal:function(siteName){
+            this.currentModal = 'modalDelete';
+            this.getModal();
+        }
+    },
+    computed:{
+        deleteSite:function(){
+            this.modalClose = !this.modalClose;
+        },
+    },
+    template:"#main"
+});
+
+
+var options = Vue.component('options-c',{
+    data:() =>{
+        return{
+
+        }
+    },
+    template:"#options"
+});
+
+var exit = Vue.component('exit-c',{
+    data:() =>{
+        return{
+
+        }
+    },
+    template:"#exit"
+});
+
+
+var routes = [
+    { path: '/', component: main },
+    { path: '/options', component: options },
+    { path: '/exit', component: exit }
+]
+
+var router = new VueRouter({
+    routes
+});
+
+var app = new Vue({
+    router
+}).$mount('#app');
+
